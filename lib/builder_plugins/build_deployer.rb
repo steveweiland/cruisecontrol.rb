f87751c90ca80eeb8a865bcfe29d8755b99442f3
rjf <rjf@SB-MacbookPro.local>
2009-09-22 16:43:56 -0700

    adding better error notification to deploy step

--- lib/builder_plugins/build_deployer.rb
+++ lib/builder_plugins/build_deployer.rb
@@ -7,6 +7,7 @@ class BuildDeployer
   def build_finished(build)
 
     system ("cd #{build.project.local_checkout}/script && ./deploy")
+    exit_code = $?
 
     File.open("/tmp/deploy-out", "r") do |infile|
       CruiseControl::Log.event( "**** Capistrano Deployment Output Begin ****")
@@ -14,6 +15,10 @@ class BuildDeployer
         CruiseControl::Log.event( line.chomp )
       end
       CruiseControl::Log.event( "**** Capistrano Deployment Output End ****")
+    end
+
+    if ( exit_code != 0 ) then
+      raise "*** Deployment failed!!!"
     end
 
   end
